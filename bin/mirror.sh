9e475f28d3fef57d6d02097d43fb8ce69c361182
Tony Bowden <tony@tmtm.com>
2021-12-02 18:32:47 +0200

    add mirroring script

---
+++ bin/mirror.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf mirror
+mkdir mirror
+cd mirror
+
+CURLOPTS='-L -c /tmp/cookies -A eps/1.2'
+
+curl $CURLOPTS -o ministries.index $(jq -r .source.url ../meta.json)
+
+for url in $(nokogiri -e "puts @doc.css('.departmentblock a/@href').map(&:text)" ministries.index); do
+  url="https://bvi.gov.vg$url"
+  curl $CURLOPTS -o $(basename $url).html $url
+done
+
+cd -
